58ecb617e5a79bd31d259982c13fac55941328cd
William George <42682665+wgeorge1990@users.noreply.github.com>
2023-04-24 02:14:16 -0400

    Add Car entity and initialize H2 database for in memory testing

---
+++ src/main/java/wg/dev/trackingserver/domain/Car.java
@@ -0,0 +1,101 @@
+package wg.dev.trackingserver.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import static jakarta.persistence.GenerationType.AUTO;
+
+@Entity
+public class Car {
+	@Id
+	@GeneratedValue(strategy= AUTO)
+	private long id;
+	
+	private String brand;
+	
+	private String model;
+	
+	private String color;
+	
+	private String registerNumber;
+	
+	private int year;
+	
+	private int price;
+	
+	public Car() {}
+	
+	public Car(String brand, 
+			String model, 
+			String color, 
+			String registerNumber, 
+			int year, 
+			int price) {
+		
+		super();
+		
+		this.brand = brand;
+		this.model = model;
+		this.color = color;
+		this.registerNumber = registerNumber;
+		this.year = year;
+		this.price = price;
+	}
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getRegisterNumber() {
+		return registerNumber;
+	}
+
+	public void setRegisterNumber(String registerNumber) {
+		this.registerNumber = registerNumber;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+}
